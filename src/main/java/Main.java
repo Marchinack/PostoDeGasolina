8ec71c13618403b62e60f4df1a1a7ffe46eda906
Mateus Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-08-08 13:24:49 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,7 @@ public class Main {
         System.out.println("2756 pessoas escolheram Diesel");
       } else {
         System.out.println("Opção inválida. Por favor, escolha novamente.");
-      }
-    }
-  }
+}
+}
+}
 }
